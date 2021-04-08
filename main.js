77389d4eecefb45697fd82a03d6d138dce0d9059
aaron <aaron.staes@gmail.com>
2021-04-08 15:38:29 +0200

    - Created onHover function: on mouse over, change color

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const container = document.querySelector('.container');
-const gridContainer = document.createElement('div');
-gridContainer.classList.add('grid-container');
 
-for (let i = 1; i < 17; i++){
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');    
-    gridItem.textContent = i;
-    gridContainer.appendChild(gridItem)
-}
+const createGrid = () => {
+    const container = document.querySelector('.container');
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
+    
+    for (let i = 1; i < 17; i++){
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.id = i; 
+        gridItem.textContent = i;
+        gridContainer.appendChild(gridItem)
+    }
+    
+    container.appendChild(gridContainer);
+};
 
-container.appendChild(gridContainer);
+const onHover = () => {
+    for (let i = 1; i < 17; i++){
+        document.getElementById(i).addEventListener('mouseover', () => {
+            document.getElementById(i).style.backgroundColor = 'red';
+        })
+    }
+};
+createGrid();
+onHover();
+
